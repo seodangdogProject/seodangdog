d5367e61018e11722ccbe560d79446f60ea1331c
위재원 <jww5555@naver.com>
2024-03-11 15:31:43 +0900

    SpringBoot 기본 폴더 생성

---
+++ seodangdogbe/src/test/java/ssafy/seodangdogbe/SeodangdogbeApplicationTests.java
@@ -0,0 +1,13 @@
+package ssafy.seodangdogbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeodangdogbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
