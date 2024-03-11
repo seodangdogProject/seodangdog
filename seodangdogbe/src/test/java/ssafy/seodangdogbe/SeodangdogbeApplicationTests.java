bf665bbd5918ec9001b727c1a24f783ad64ba370
김아림 <kal72222222@gmail.com>
2024-03-11 20:48:11 +0900

    fix : SpringBootTest 주석처리해서 build 시 application.propertiese 못 찾는 에러 잡기

--- seodangdogbe/src/test/java/ssafy/seodangdogbe/SeodangdogbeApplicationTests.java
+++ seodangdogbe/src/test/java/ssafy/seodangdogbe/SeodangdogbeApplicationTests.java
@@ -3,7 +3,7 @@ package ssafy.seodangdogbe;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class SeodangdogbeApplicationTests {
 
 	@Test
