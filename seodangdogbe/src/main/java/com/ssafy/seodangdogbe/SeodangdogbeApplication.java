461c8042f7d402c75afd77ecd87be47e91e92e01
김창희 <rlackdgml97@naver.com>
2024-03-12 14:59:23 +0900

    240312 changhee fix: gradle 변경

---
+++ seodangdogbe/src/main/java/com/ssafy/seodangdogbe/SeodangdogbeApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.seodangdogbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeodangdogbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeodangdogbeApplication.class, args);
+	}
+
+}
