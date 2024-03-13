0bc8301990c6b87ddf593e78ad7eb3ace4f2e7cd
김아림 <kal72222222@gmail.com>
2024-03-13 09:59:35 +0900

    env : html 파일 복사-붙여넣기 삭제 -> localhost:3000으로 바로 포워딩

--- seodangdogfe/next.config.mjs
+++ seodangdogfe/next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    distDir: 'build',
-    output: 'standalone',
+
 };
 
+// module.exports = {
+//     typescript : {
+//         ignoreBuildErrors : true,  // typeScript 타입 에러시 build 안되는거 무시하기
+//     }
+// }
 export default nextConfig;
