546436d2e915b1e16a9f8b3d78e14d605888f58e
양윤모 <yym4481@naver.com>
2024-03-18 01:00:41 +0900

    240318 feat : 뉴스디테일 컴포넌트 구현중

---
+++ seodangdogfe/src/containers/NewsDetail.tsx
@@ -0,0 +1,43 @@
+import styled from "./NewsDetail.module.css";
+import classNames from "classnames/bind";
+export default function newsDetail() {
+	const cx = classNames.bind(styled);
+	return (
+		<>
+			<div className={cx("container")}>
+				<section className={cx("news-content")}>
+					<h1 className={cx("title")}>
+						기아 전기차 라인업 한눈에…'EV 트렌드 코리아 2024' 참가
+					</h1>
+					<div className={cx("date")}>2024.03.06. 오전 11:37</div>
+					<div className={cx("hashtag")}>
+						<div className={cx("hashtag-item")}>전기차</div>
+						<div className={cx("hashtag-item")}>전기차</div>
+						<div className={cx("hashtag-item")}>전기차</div>
+						<div className={cx("hashtag-item")}>전기차</div>
+						<div className={cx("hashtag-item")}>전기차</div>
+						<div className={cx("hashtag-item")}>전기차</div>
+					</div>
+					<div className={cx("content")}>
+						<div className={cx("img")}>
+							<img
+								src="https://imgnews.pstatic.net/image/015/2024/03/17/0004960840_001_20240318003801027.jpg?type=w647"
+								alt="asd"
+							/>
+						</div>
+						<div className={cx("text")}>
+							기아가 'EV 트렌드 코리아 2024'에 참가해 새로운
+							모빌리티 시대의 경험을 선보인다. △전기차 충전 △공간
+							및 신기술 △지속가능성 등을 주제로 3개의 전시 공간을
+							마련했다.  올해로 7회차를 맞은 EV 트렌드 코리아는
+							전기차 민간보급 확대와 새로운 전기차 문화 형성을
+							위해 정부 주도하에 열리는 서울 유일의 전기차
+							엑스포로 이날부터 8일까지 서울
+						</div>
+					</div>
+				</section>
+				<section className={cx("quiz")}></section>
+			</div>
+		</>
+	);
+}
