10a923627f2b554583b5ac5bc3b224813c256867
김창희 <rlackdgml97@naver.com>
2024-03-18 12:03:05 +0900

    240318 창희 feat: 추천 라우터 개발

--- seodangdogml/src/main.py
+++ seodangdogml/src/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from recommend.content_base_recommend import router as content_base_recommend_router
 
 app = FastAPI()
+
 
 @app.get("/hello")
 def hello():
     return {"message": "Hello! FastAPI!!"}
+
+
+app.include_router(content_base_recommend_router)
