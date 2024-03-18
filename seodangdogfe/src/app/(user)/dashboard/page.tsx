f858d5356ee0f996e6062eaef63eb1067a23a4be
양윤모 <yym4481@naver.com>
2024-03-18 17:51:00 +0900

    240318 윤모 design : 대시보드 파일 생성

---
+++ seodangdogfe/src/app/(user)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import DashboardContainer from "@/containers/DashboardContainer";
+
+export default function Dashboard() {
+  return (
+    <>
+      <DashboardContainer />
+    </>
+  );
+}
---
+++ seodangdogfe/src/containers/DashboardContainer.tsx
@@ -0,0 +1,6 @@
+import styled from "./DashboardContainer.module.css";
+import classNames from "classnames/bind";
+export default function DashboardContainer() {
+  const cd = classNames.bind(styled);
+  return <>대쉬보드다</>;
+}
