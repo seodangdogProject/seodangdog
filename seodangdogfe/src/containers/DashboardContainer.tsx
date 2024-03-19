73de1c95ab53067c271e6a8ec726039358a61cc9
양윤모 <yym4481@naver.com>
2024-03-19 12:28:55 +0900

    240319 윤모 design : 대쉬보드 컨테이너 레이아웃 작성

--- seodangdogfe/src/containers/DashboardContainer.tsx
+++ seodangdogfe/src/containers/DashboardContainer.tsx
@@ -1,6 +1,38 @@
 import styled from "./DashboardContainer.module.css";
 import classNames from "classnames/bind";
+function Info() {
+  return <>asdf</>;
+}
+function Chart() {
+  return <>1234</>;
+}
+function RecentPreview() {
+  return <></>;
+}
+function State() {
+  return <></>;
+}
 export default function DashboardContainer() {
-  const cd = classNames.bind(styled);
-  return <>대쉬보드다</>;
+  const cx = classNames.bind(styled);
+  return (
+    <>
+      <div className={cx("container")}>
+        <div className={cx("dashboard")}>
+          <div className={cx("dashboard__first")}>
+            <div className={cx("dashboard__second", ["bg-light-purple"])}>
+              <Info />
+            </div>
+            <div className={cx("dashboard__second", ["bg-light-purple"])}>
+              <Chart />
+            </div>
+          </div>
+          {/* 두 번째 */}
+          <div className={cx("dashboard__first", "$bg__light__purple")}>
+            <RecentPreview />
+            <State />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
