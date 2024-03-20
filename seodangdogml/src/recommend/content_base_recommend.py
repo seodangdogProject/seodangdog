555dc02e5bb6b302fb17be5f4744138225a0df40
김창희 <rlackdgml97@naver.com>
2024-03-20 10:28:56 +0900

    240320 창희 feat: 라우터변경

--- seodangdogml/src/recommend/content_base_recommend.py
+++ seodangdogml/src/recommend/content_base_recommend.py
@@ -40,7 +40,7 @@ for news in allNews:
     title = news['newsTitle']
     news_data_objects.append(News(id, title))
 
-@router.get("/cbf_recom")
+@router.get("/fast/cbf_recom")
 # def process_data(item: Item):
 def process_data():
     # keyword_weights = {"北 해킹 의혹": 16.0, "대법원": 1.1, "EU": 1.3, "애플": 2.0}
