69a6581758a902045330e7fcaa994546b922bb5f
양윤모 <yym4481@naver.com>
2024-03-20 09:46:53 +0900

    240320 윤모 design : 최신본 뉴스 리스트 디테일 페이지 UI 구현

--- seodangdogfe/src/components/dashboard/LastNewsList.tsx
+++ seodangdogfe/src/components/dashboard/LastNewsList.tsx
@@ -7,6 +7,27 @@ function CardNews() {
         src="https://thumb.mt.co.kr/06/2024/03/2024030610501730820_1.jpg/dims/optimize/"
         alt=""
       />
+      <div className={styled.desc}>
+        <div className={styled.title}>
+          기아 전기차 라인업 한눈에…'EV 트렌드 코리아 2024' 참가
+        </div>
+        <div className={styled.meta}>조회수 1만회 • 3 시간 전</div>
+        <div className={styled.company}>
+          <img
+            src="https://mimgnews.pstatic.net/image/upload/office_logo/018/2018/08/08/logo_018_57_20180808174308.png"
+            alt=""
+          />
+        </div>
+        <div className={styled.summary}>
+          기아가 'EV 트렌드 코리아 2024'에 참가해 새로운 모빌리티 시대의 경험을
+          선보인다. △전기차 충전 △공간 및 신기술 △지속가능성 등을...
+        </div>
+        <ul className={styled.hashtag}>
+          <li className={styled.item}># 은행</li>
+          <li className={styled.item}># 자동차</li>
+          <li className={styled.item}># 기아</li>
+        </ul>
+      </div>
     </div>
   );
 }
