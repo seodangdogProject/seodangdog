6c22cc948475433ecc8828525d1e2db4e0b7bed0
김아림 <kal72222222@gmail.com>
2024-03-21 13:39:20 +0900

    240321 아림 feat : api 뭍이기

--- seodangdogbe/src/main/java/com/ssafy/seodangdogbe/test/testController.java
+++ seodangdogbe/src/main/java/com/ssafy/seodangdogbe/test/testController.java
@@ -10,15 +10,12 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
 @Slf4j
-
+@RequestMapping("/api")
 public class testController {
 
     private final UserService userService;
