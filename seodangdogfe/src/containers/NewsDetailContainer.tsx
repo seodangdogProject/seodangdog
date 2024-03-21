15489d726e56ac3c505859016dfee18be0c8d1c1
양윤모 <yym4481@naver.com>
2024-03-21 11:26:10 +0900

    240321 윤모 feat : 뉴스디테일 페이지 데이터 가져와서 뿌려주기 기능 구현

--- seodangdogfe/src/containers/NewsDetailContainer.tsx
+++ seodangdogfe/src/containers/NewsDetailContainer.tsx
@@ -4,14 +4,31 @@ import styled from "./NewsDetailContainer.module.css";
 import classNames from "classnames/bind";
 import Quiz from "@/components/newsDetail/Quiz";
 import Cover from "@/components/newsDetail/Cover";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 export default function NewsDetailContainer() {
   const [currentQuestion, setCurrentQuestion] = useState<number>(0);
   const cx = classNames.bind(styled);
+
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    (async () => {
+      const a = await fetch("https://j10e104.p.ssafy.io/api/news/11355", {
+        method: "get",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhcmltIiwiVVNFUiI6IlJPTEVfVVNFUiIsImV4cCI6MTcxMzU3ODcxNX0.6iCO_VO6jdC-fvfceiQtN6kyFqInb74AUBC-I4ZUYkg",
+        },
+      });
+      const dt = await a.json();
+      setData(dt);
+    })();
+  }, []);
   return (
     <>
       <div className={cx("container")}>
-        <NewsContent />
+        <NewsContent data={data} />
         {currentQuestion === 0 ? (
           <Cover goNext={setCurrentQuestion} />
         ) : (
