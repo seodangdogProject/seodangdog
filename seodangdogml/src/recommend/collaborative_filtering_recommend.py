9044542a5f23f892de89357eb0452b563b8ab965
김창희 <rlackdgml97@naver.com>
2024-03-22 16:29:02 +0900

    240322 창희 feat: 필터업데이트

--- seodangdogml/src/recommend/collaborative_filtering_recommend.py
+++ seodangdogml/src/recommend/collaborative_filtering_recommend.py
@@ -11,6 +11,119 @@ from .content_base_recommend import make_user_news_df
 
 router = APIRouter()
 
+content = [
+    {
+        'news_id': 'A',
+        'title': 'A뉴스'
+    },
+    {
+        'news_id': 'B',
+        'title': 'B뉴스'
+    },
+    {
+        'news_id': 'C',
+        'title': 'C뉴스'
+    },
+    {
+        'news_id': 'D',
+        'title': 'D뉴스'
+    },
+    {
+        'news_id': 'E',
+        'title': 'E뉴스'
+    },
+    {
+        'news_id': 'F',
+        'title': 'F뉴스'
+    },
+    {
+        'news_id': 'G',
+        'title': 'G뉴스'
+    },
+    {
+        'news_id': 'H',
+        'title': 'H뉴스'
+    },
+    {
+        'news_id': 'I',
+        'title': 'I뉴스'
+    },
+    {
+        'news_id': 'J',
+        'title': 'J 뉴스'
+    }
+]
+test = [
+        {
+            'user_id': 1,
+            'news_id': 'F',
+            'rating': 7
+        },
+        {
+            'user_id': 2,
+            'news_id': 'A',
+            'rating': 3
+        },
+        {
+            'user_id': 2,
+            'news_id': 'B',
+            'rating': 3
+        },
+        {
+            'user_id': 3,
+            'news_id': 'A',
+            'rating': 3
+        },
+        {
+            'user_id': 3,
+            'news_id': 'B',
+            'rating': 3
+        },
+        {
+            'user_id': 4,
+            'news_id': 'B',
+            'rating': 5
+        },
+        {
+            'user_id': 4,
+            'news_id': 'C',
+            'rating': 5
+        },
+        {
+            'user_id': 6,
+            'news_id': 'F',
+            'rating': 20
+        },
+        {
+            'user_id': 6,
+            'news_id': 'H',
+            'rating': 20
+        },
+        {
+            'user_id': 6,
+            'news_id': 'I',
+            'rating': 20
+        },
+        {
+            'user_id': 6,
+            'news_id': 'J',
+            'rating': 20
+        }
+]
+test_col = ['user_id','news_id','rating']
+test_df = pd.DataFrame(test, columns=test_col)
+
+content_df =['news_id','title']
+content_df = pd.DataFrame(content, columns=content_df)
+
+print('test_df')
+print(test_df)
+
+print('content_df')
+print(content_df)
+test_df.to_csv('ratings2.csv', sep='|', index=False)
+content_df.to_csv('news2.csv', sep='|', index=False)
+
 # 뉴스 데이터프레임
 news = {'news_id': [news.id for news in news_data_objects],
         'title': [news.title for news in news_data_objects]}
