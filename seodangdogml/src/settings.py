9eaaf5a54c1c86c67b62c52eef5c489a3c27d550
박찬영 <albam6912@naver.com>
2024-03-25 17:44:06 +0900

    240325 찬영 feat: 환경파일 settings.py 추가

---
+++ seodangdogml/src/settings.py
@@ -0,0 +1,9 @@
+import my_settings
+
+S3_BUCKET_NAME = "seodangdog-s3"
+S3_LOCATION = "ap-northeast-2"
+S3_ACCESS_KEY = my_settings.S3_ACCESS_KEY
+S3_SECRET_KEY = my_settings.S3_SECRET_KEY
+
+WC_FONT_PATH = "C:\\Users\\SSAFY\\AppData\\Local\\Microsoft\\Windows\\Fonts\\NanumMyeongjoExtraBold.ttf"
+
