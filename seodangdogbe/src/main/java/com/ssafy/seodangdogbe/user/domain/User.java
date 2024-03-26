d11ded625e563025411b8522a0d827b6ce68e9cd
jaewonwi <jww5555@naver.com>
2024-03-26 22:07:22 +0900

    240326 재원 fix: isDelete Setter 중복제거

--- seodangdogbe/src/main/java/com/ssafy/seodangdogbe/user/domain/User.java
+++ seodangdogbe/src/main/java/com/ssafy/seodangdogbe/user/domain/User.java
@@ -13,6 +13,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Arrays;
+
 @Entity
 @NoArgsConstructor
 @Data
