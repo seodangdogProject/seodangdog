9219b95570ba97228969f3bbc565860294a324f9
양윤모 <yym4481@naver.com>
2024-03-27 14:38:08 +0900

    240327 윤모 fix : 뉴스 디테일 받아온 newsSeq로 데이터패칭하기 구현

--- seodangdogfe/src/containers/NewsDetailContainer.tsx
+++ seodangdogfe/src/containers/NewsDetailContainer.tsx
@@ -6,16 +6,19 @@ import Quiz from "@/components/newsDetail/Quiz";
 import Cover from "@/components/newsDetail/Cover";
 import { useEffect, useState } from "react";
 import { privateFetch } from "@/utils/http-commons";
+import { usePathname } from "next/navigation";
 export default function NewsDetailContainer() {
   const [currentQuestion, setCurrentQuestion] = useState<number>(0);
   const cx = classNames.bind(styled);
 
   const [data, setData] = useState({});
+  const pathname = usePathname();
 
   useEffect(() => {
+    console.log(pathname.split("/")[2]);
     // 데이터 받아오는 함수 START
     (async () => {
-      const res = await privateFetch("/news/16964", "GET");
+      const res = await privateFetch("/news/" + pathname.split("/")[2], "GET");
       if (res.status === 200) setData(await res.json());
       else {
         console.log("error 발생");
