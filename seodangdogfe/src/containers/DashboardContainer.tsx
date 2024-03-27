0a424b0664db8251c24c6805bbc8e3133d439cdd
양윤모 <yym4481@naver.com>
2024-03-27 16:10:26 +0900

    240327 윤모 design : 대시보드 UI 비율 수정

--- seodangdogfe/src/containers/DashboardContainer.tsx
+++ seodangdogfe/src/containers/DashboardContainer.tsx
@@ -5,91 +5,75 @@ import UserCard from "@/components/dashboard/UserCard";
 import Strict from "@/components/strict/strict";
 import Chart from "@/components/chart/chart";
 function Info() {
-    return <></>;
+  return <></>;
 }
 
 // function RecentPreview() {
 //   return <></>;
 // }
 function State() {
-    return <></>;
+  return <></>;
 }
 export default function DashboardContainer() {
-    const cx = classNames.bind(styled);
-    return (
-        <>
-            <div className={cx("container")}>
-                <div className={cx("dashboard")}>
-                    <div className={cx("dashboard__top")}>
-                        <div
-                            className={cx(
-                                "dashboard__top__first",
-                                "padding-item",
-                                ["bg-light-purple"]
-                            )}
-                        >
-                            <div className={styled.title}>Info</div>
-                            <div className={cx("user", ["box-shodow-custom"])}>
-                                <UserCard />
-                            </div>
-                            <div
-                                className={cx("streak", ["box-shodow-custom"])}
-                            >
-                                <Strict></Strict>
-                            </div>
-                        </div>
-                        <div
-                            className={cx(
-                                "dashboard__top__second",
-                                "padding-item",
-                                ["bg-light-purple"]
-                            )}
-                        >
-                            <div className={styled.title}>워드클라우드</div>.
-                            <div
-                                className={cx("wordcloud", [
-                                    "box-shodow-custom",
-                                ])}
-                            >
-                                <div
-                                    className={cx("boxshadow", [
-                                        "box-shodow-custom",
-                                    ])}
-                                >
-                                    {/* 여기에 워드클라우드 컴포넌트 */}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    {/* 두 번째 */}
-                    <div
-                        className={cx("dashboard__bottom", "bg__light__purple")}
-                    >
-                        <div
-                            className={cx(
-                                "dashboard__bottom__first",
-                                ["bg-light-purple"],
-                                "padding-item"
-                            )}
-                        >
-                            <RecentNewsPreview />
-                        </div>
-                        <div
-                            className={cx(
-                                "dashboard__bottom__second",
-                                ["bg-light-purple"],
-                                "padding-item"
-                            )}
-                        >
-                            <div className={styled.title}>능력치</div>.
-                            <div className={cx("state", ["box-shodow-custom"])}>
-                                <Chart />
-                            </div>
-                            <State />
-                        </div>
-                    </div>
-                </div>
+  const cx = classNames.bind(styled);
+  return (
+    <>
+      <div className={cx("container")}>
+        <div className={cx("dashboard")}>
+          <div className={cx("dashboard__top")}>
+            <div
+              className={cx("dashboard__top__first", "padding-item", [
+                "bg-light-purple",
+              ])}
+            >
+              <div className={styled.title}>Info</div>
+              <div className={cx("user", ["box-shodow-custom"])}>
+                <UserCard />
+              </div>
+              <div className={cx("streak", ["box-shodow-custom"])}>
+                <Strict></Strict>
+              </div>
             </div>
-        </>
-    );
+            <div
+              className={cx("dashboard__top__second", "padding-item", [
+                "bg-light-purple",
+              ])}
+            >
+              <div className={styled.title}>워드클라우드</div>
+              <div className={cx("wordcloud", ["box-shodow-custom"])}>
+                <div className={cx("boxshadow", ["box-shodow-custom"])}>
+                  {/* 여기에 워드클라우드 컴포넌트 */}
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* 두 번째 */}
+          <div className={cx("dashboard__bottom", "bg__light__purple")}>
+            <div
+              className={cx(
+                "dashboard__bottom__first",
+                ["bg-light-purple"]
+                // "padding-item"
+              )}
+            >
+              <RecentNewsPreview />
+            </div>
+            <div
+              className={cx(
+                "dashboard__bottom__second",
+                ["bg-light-purple"],
+                "padding-item"
+              )}
+            >
+              <div className={styled.title}>능력치</div>
+              <div className={cx("state", ["box-shodow-custom"])}>
+                <Chart />
+              </div>
+              <State />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
