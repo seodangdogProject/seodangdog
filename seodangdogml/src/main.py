1a2dac500f858042cd67ad301a10e8632ddef9f0
박찬영 <albam6912@naver.com>
2024-03-27 14:56:02 +0900

    240327 찬영 feat: 뉴스 크롤링 테스트 코드 작성

--- seodangdogml/src/main.py
+++ seodangdogml/src/main.py
@@ -1,17 +1,55 @@
+import sched
+
 from fastapi import FastAPI
+from apscheduler.schedulers.background import BackgroundScheduler
+from contextlib import asynccontextmanager
+import uvicorn
+
+# 사용자 라이브러리 추가
 from recommend.cbf_recommend import router as cbf_router
 from recommend.cf_recommend import router as cf_router
 from repository.news_repository import router as news_repo_router
 from repository.recommend_repository import router as recommend_repo_router
 from recommend.mf_train import router as mf_train_router
 from recommend.mf_recommend import router as mf_recommend_router
+from repository.news_repository import save_news, mysql_save
+from crawling.news_crawling import crawling_main, test
 
+
+back_scheduler = BackgroundScheduler(timezone='Asia/Seoul')
+
+
+@back_scheduler.scheduled_job('cron', daily="1/1", id='test')
+
+# @back_scheduler.scheduled_job('cron', minute="36", id='test')
+def scheduled_job():
+    crawling_main()
+    save_news()
+    mysql_save()
+
+
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    back_scheduler.start()
+    yield
+
+
+# app = FastAPI(lifespan=lifespan)
 app = FastAPI()
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=5000)
 
 
 @app.get("/fast/hello")
 def hello():
     return {"message": "Hello! FastAPI!!"}
+
+@app.get("/fast/chrome_test")
+def chrome_test():
+    test()
+    return {"message": "chrome_test activated."}
 
 
 app.include_router(cbf_router)
