a5ce574eb99faabebb283fb378dac40360cfdab9
양윤모 <yym4481@naver.com>
2024-03-28 11:21:10 +0900

    240328 윤모 design : 퀴즈컴포넌트 UI 구현

--- seodangdogfe/src/components/newsDetail/Quiz.tsx
+++ seodangdogfe/src/components/newsDetail/Quiz.tsx
@@ -4,26 +4,42 @@ export default function Quiz() {
   const cx = classNames.bind(styled);
   return (
     <>
-      <div className={cx("container")}>
-        <h4 className={cx("question")}>
-          <div>1.</div>
-          <div>위 기사에서 언급된 “수급상황실”은 무엇을 의미합니까?</div>
-        </h4>
-        <div className={cx("case-container")}>
-          <div className={cx("case")}>(A) 공급량 조절을 위한 부서</div>
-          <div className={cx("case")}>(B) 긴급한 재고 확인 장소</div>
-          <div className={cx("case")}>
-            (C) 물품 수요 및 공급 상활을 모니터링하는 시설
-          </div>
-          <div className={cx("case")}>(D) 가격 변동에 따른 수요 조절기구</div>
-        </div>
-        <div className={cx("btn-container")}>
-          <button className={cx("next-btn")}>
-            <div>다음</div>
-            <div className={cx("next-icon")}>
-              <img src="/next-icon.svg" alt="" />
+      <div className={cx("quiz-container")}>
+        <div className={cx("quiz-content")}>
+          <h4 className={cx("question")}>
+            <div>1.</div>
+            <div>위 기사에서 언급된 “수급상황실”은 무엇을 의미합니까?</div>
+          </h4>
+          <div className={cx("case-container")}>
+            <div className={cx("case")}>
+              <div className={cx("case__number")}>(A)</div>
+              <div className={cx("case__item")}>공급량 조절을 위한 부서</div>
             </div>
-          </button>
+            <div className={cx("case")}>
+              <div className={cx("case__number")}>(A)</div>
+              <div className={cx("case__item")}>긴급한 재고 확인 장소</div>
+            </div>
+            <div className={cx("case")}>
+              <div className={cx("case__number")}>(A)</div>
+              <div className={cx("case__item")}>
+                물품 수요 및 공급 상활을 모니터링하는 시설
+              </div>
+            </div>
+            <div className={cx("case")}>
+              <div className={cx("case__number")}>(A)</div>
+              <div className={cx("case__item")}>
+                가격 변동에 따른 수요 조절기구
+              </div>
+            </div>
+          </div>
+          <div className={cx("btn-container")}>
+            <button className={cx("next-btn")}>
+              <div>다음</div>
+              <div className={cx("next-icon")}>
+                <img src="/next-icon.svg" alt="" />
+              </div>
+            </button>
+          </div>
         </div>
       </div>
     </>
