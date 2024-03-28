a75c6bdc7319eb9535771bd20fc47668158a0346
양윤모 <yym4481@naver.com>
2024-03-28 16:53:32 +0900

    240328 윤모 feat : 형광펜 모달 기능 구현중

--- seodangdogfe/src/components/newsDetail/NotSolved.tsx
+++ seodangdogfe/src/components/newsDetail/NotSolved.tsx
@@ -3,6 +3,7 @@ import Cover from "./Cover";
 import Quiz from "./Quiz";
 import Summary from "./Summary";
 import { Dispatch, SetStateAction } from "react";
+import Highlighter from "@/assets/highlighter-icon.svg";
 export default function NotSolved({
   data,
   currentQuizNumber,
@@ -21,6 +22,17 @@ export default function NotSolved({
   return (
     <div className={cx("container")}>
       <div className={cx("detail-container", ["box-shodow-custom"])}>
+        <div className={cx("pentool", ["box-shodow-custom"])}>
+          <div className={cx("pentool__item")}>
+            <img src="/highlighter-icon.svg" alt="" />
+          </div>
+          <div className={cx("pentool__item")}>
+            <img src="/search-icon.svg" alt="" />
+          </div>
+          <div className={cx("pentool__item")}>
+            <img src="/eraser-icon.svg" alt="" />
+          </div>
+        </div>
         <NewsContent keywords={keywords} data={data} />
         {currentQuizNumber === 0 ? (
           <Cover setCurrentQuiz={setCurrentQuizNumber} />
