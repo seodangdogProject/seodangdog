83caf7f712ff54b8aabd2b85090c1387660b94a9
김아림 <kal72222222@gmail.com>
2024-03-28 09:27:22 +0900

    240328 아림 feat : lose weight dto 변경

--- seodangdogbe/src/main/java/com/ssafy/seodangdogbe/keyword/controller/KeywordController.java
+++ seodangdogbe/src/main/java/com/ssafy/seodangdogbe/keyword/controller/KeywordController.java
@@ -1,11 +1,14 @@
 package com.ssafy.seodangdogbe.keyword.controller;
 
 import com.ssafy.seodangdogbe.auth.service.UserService;
+import com.ssafy.seodangdogbe.common.MessageResponseDto;
 import com.ssafy.seodangdogbe.keyword.dto.NewsRefreshReqDto;
 import com.ssafy.seodangdogbe.keyword.service.KeywordService;
 import com.ssafy.seodangdogbe.news.service.NewsService;
 import com.ssafy.seodangdogbe.user.domain.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,18 +22,19 @@ public class KeywordController {
     public final NewsService newsService;
     public final KeywordService keywordService;
 
-    @PostMapping("/keyword") // controller 어떻게 맞추기로 했는 지모루겠어서 둘게욤 ~
+    @PostMapping("/keyword/click")
     public void addWeight(@RequestBody List<String> keywords){
         int userSeq = userService.getUserSeq();
         User user = userService.getUser();
         keywordService.addKeywordListWeight(user, keywords, 1.28);
     }
 
-    @PostMapping("/keyword/lose")
-    public void minusWeight(@RequestBody List<NewsRefreshReqDto> newsRefreshReqDtoList){
+    @PostMapping("/keyword/refre")
+    public ResponseEntity<MessageResponseDto> minusWeight(@RequestBody List<NewsRefreshReqDto> newsRefreshReqDtoList){
         int userSeq = userService.getUserSeq();
         User user = userService.getUser();
-        keywordService.minusKeywordListWeight(user, newsRefreshReqDtoList, -0.35, -0.2);
+        MessageResponseDto result = keywordService.minusKeywordListWeight(user, newsRefreshReqDtoList, -0.35, -0.2);
+        return ResponseEntity.ok(result);
     }
 
 }
