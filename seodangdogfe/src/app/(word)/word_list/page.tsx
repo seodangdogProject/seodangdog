b4dbee913cef809e25337a6c9a799db44fbf5058
김아림 <kal72222222@gmail.com>
2024-04-01 09:32:36 +0900

    240401 아림 feat : 이미지 정리

--- seodangdogfe/src/app/(word)/word_list/page.tsx
+++ seodangdogfe/src/app/(word)/word_list/page.tsx
@@ -1,13 +1,7 @@
 "use client";
 // WordGame.tsx
-import React, {
-    useState,
-    useEffect,
-} from "react";
-import {
-    useRecoilValue,
-    useSetRecoilState,
-} from "recoil";
+import React, { useState, useEffect } from "react";
+import { useRecoilValue, useSetRecoilState } from "recoil";
 import styles from "./wordlist_layout.module.css";
 import {
     koWordListState,
@@ -18,6 +12,8 @@ import SearchIcon from "../../../assets/search-icon.svg";
 import WordDetailModal from "../../../components/wordComponent/wordDetailModal";
 import { privateFetch } from "@/utils/http-commons";
 import TrashCanIcon from "@/assets/delete-trashcan-icon.svg";
+import ToastPopup from "@/components/toast/Toast";
+
 const OneWord: React.FC = () => {
     const engWordList = useRecoilValue(engWordListState);
     const koWordList = useRecoilValue(koWordListState);
@@ -30,6 +26,7 @@ const OneWord: React.FC = () => {
     const [searchText, setSearchText] = useState("");
     const setKoWordList = useSetRecoilState(koWordListState);
     const setEngWordList = useSetRecoilState(engWordListState);
+    const [toast, setToast] = useState(false);
 
     useEffect(() => {
         // 데이터 받아오는 함수 START
@@ -103,6 +100,7 @@ const OneWord: React.FC = () => {
                 console.log("error 발생");
             }
         })();
+        setToast(true);
         initReq(); // 다시 요청하기
     };
 
@@ -145,6 +143,16 @@ const OneWord: React.FC = () => {
                         clickedWord={clickedWord}
                     ></WordDetailModal>
                 )}
+
+                <div style={{ marginLeft: "50px" }}>
+                    {toast && (
+                        <ToastPopup
+                            setToast={setToast}
+                            message={"삭제가 완료되었습니다."}
+                        />
+                    )}
+                </div>
+
                 <div className={styles.content_cotainer}>
                     <div className={styles.search_cotinaer}>
                         <input
